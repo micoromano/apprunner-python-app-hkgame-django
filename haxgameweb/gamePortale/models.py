8b059fd6d12ce4a963c00ccdd2b4dc1af7a94e10
Marco Camelin <marco.camelin@accenture.com>
2023-04-11 01:05:46 +0200

    .

---
+++ haxgameweb/gamePortale/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+
+class info(models.Model):
+    id = models.CharField(max_length=30,primary_key=True)
+    nome = models.CharField(max_length=30)
+    cognome = models.CharField(max_length=30)
+    attivo = models.CharField(max_length=30)
+    username = models.CharField(max_length=30)
+    password = models.CharField(max_length=30)
+    rank = models.IntegerField
+
+class UserItem(models.Model):
+    id = models.CharField(max_length=30,primary_key=True)
+    Info = models.ForeignKey(info,on_delete=models.CASCADE)
+
+
