361863abc947e5c8b6e039d2b651b2d56df39693
micoromano <marco.camelin@gmail.com>
2023-04-17 12:53:20 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 # start-server.sh
+
+python manage.py migrate
+
 if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ] ; then
     (python3 manage.py createsuperuser --no-input)
 fi
