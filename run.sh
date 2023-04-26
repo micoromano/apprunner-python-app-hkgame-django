7293699fef499044dbfe07f050da6bd333280abc
Marco Camelin <marco.camelin@accenture.com>
2023-04-26 13:11:05 +0200

    add lev1 graphics

--- run.sh
+++ run.sh
@@ -6,6 +6,6 @@ python manage.py migrate
 if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ] ; then
     (python3 manage.py createsuperuser --no-input)
 fi
-(python3 manage.py runserver 8080) &
+(python3 manage.py runserver 8082) &
 nginx -g "daemon off;"
 python manage.py migrate
